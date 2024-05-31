02bb8717f308c39871eeb231f160ec667d7efc86
taeheon9625 <xogjs9625@naver.com>
2024-05-31 17:27:17 +0900

    [태헌] 마이페이지 구현

---
+++ bbanggood/src/Components/MyPage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../CSS/MyPage.css';
+import logo from '../images/BBanggood_logo_white_line.png'; // 기존 로고
+import profileImage from '../images/호빵맨.png'; // 새로운 프로필 이미지
+const MyPage = () => {
+    return (
+        <div className="my-page">
+            <div className="my-page-container">
+                <div className="my-page-header">
+                    <img src={logo} alt="Logo" className="my-page-logo" />
+                    <h2>회원 정보</h2>
+                </div>
+                <div className="my-page-content">
+                    <div className="my-page-info">
+                    <img src={profileImage} alt="Profile" className="profile-img" />
+                        <div className="info-text">
+                            <p>이름: 이주원</p>
+                            <p>셋탑번호: 2001920</p>
+                            <p>핸드폰 번호: 01012345678</p>
+                            <p>성별: 여성</p>
+                        </div>
+                    </div>
+                    <div className="my-page-actions">
+                        <button className="action-button">개인 정보 수정</button>
+                        <button className="action-button">성인인증</button>
+                        <button className="action-button">로그아웃</button>
+                        <button className="action-button">회원 탈퇴</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MyPage;
