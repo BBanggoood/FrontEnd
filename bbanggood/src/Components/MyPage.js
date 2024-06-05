b2e3b90deda27eac2496c40bea2c4033bac9d9eb
taeheon9625 <xogjs9625@naver.com>
2024-06-05 11:11:33 +0900

    [태헌] 마이페이지 수정

--- bbanggood/src/Components/MyPage.js
+++ bbanggood/src/Components/MyPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../CSS/MyPage.css';
-import logo from '../images/BBanggood_logo_white_line.png'; // 기존 로고
 import profileImage from '../images/호빵맨.png'; // 새로운 프로필 이미지
 
 const MyPage = () => {
@@ -14,13 +13,15 @@ const MyPage = () => {
     return (
         <div className="my-page">
             <div className="my-page-container">
-                <div className="my-page-header">
-                    <img src={logo} alt="Logo" className="my-page-logo" />
-                    <h2>회원 정보</h2>
+                <div className="my-page-welcome">
+                    <div className="welcome-text">
+                        <h3>HELLO, BBANGGOOD !</h3>
+                        <button className="edit-button">개인 정보 수정</button>
+                    </div>
+                    <img src={profileImage} alt="Profile" className="welcome-img" />
                 </div>
-                <div className="my-page-content">
+                <div className="my-page-main">
                     <div className="my-page-info">
-                        <img src={profileImage} alt="Profile" className="profile-img" />
                         <div className="info-text">
                             <p>이름: 이주원</p>
                             <p>셋탑번호: 2001920</p>
@@ -29,7 +30,6 @@ const MyPage = () => {
                         </div>
                     </div>
                     <div className="my-page-actions">
-                        <button className="action-button">개인 정보 수정</button>
                         <button className="action-button" onClick={handleAdultVerification}>성인인증</button>
                         <button className="action-button">로그아웃</button>
                         <button className="action-button">회원 탈퇴</button>
